1a6bdecb66229b5a648e23856672b5df43d111d7
Soukaina Chrit <81932046+soukainachrit@users.noreply.github.com>
2022-06-11 12:36:48 +0200

    Delete IFileUploaderService.java

--- src/main/java/com/gsnotes/services/IFileUploaderService.java
+++
@@ -1,8 +0,0 @@
-package com.gsnotes.services;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface IFileUploaderService {
-    public void uploadFile(MultipartFile file);
-
-}
